6a9dfd5ace07a848ccde3ee52eedcb7ccf2134fb
Charles <k22009879@kcl.ac.uk>
2023-09-26 19:04:15 +0100

    solution update

--- primes.py
+++ primes.py
@@ -2,5 +2,30 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    base = 2
     list = []
+    if number_of_primes == 1:
+        list.append(base)
+        return list
+    else:
+        list.append(base)
+        while len(list)<number_of_primes:
+
+            base = base+1
+            if isPrime(base):
+                list.append(base)
+    print(list)
+
+
+
+
     return list
+
+def isPrime(num):
+    if num ==1:
+        return False
+    elif num > 1:
+        for i in range (2, num):
+            if num%i == 0:
+                return False
+    return True
